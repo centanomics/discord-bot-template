78e41427b01fb112fc9ffabb6788feec0633bce0
semyers189 <semyers@fullsail.edu>
2021-10-15 23:35:03 -0400

    created bot file and public folder

---
+++ bot.js
@@ -0,0 +1,15 @@
+// setups .env files with tokens
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Bot Template listening at http://localhost:${port}`);
+});
