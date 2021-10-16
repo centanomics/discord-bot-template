7a365116b7124db1a4135b1324dfd3f1a00b9930
semyers189 <semyers@fullsail.edu>
2021-10-15 23:42:24 -0400

    started on discord bot stuffs

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Sets up express server for hosting reasons
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
@@ -13,3 +14,28 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Bot Template listening at http://localhost:${port}`);
 });
+
+// Start Bot Code
+
+const Discord = require('discord.js');
+const connectDB = require('./config/db');
+
+const intents = [
+  'GUILDS',
+  'GUILD_MEMBERS',
+  'GUILD_BANS',
+  'GUILD_INVITES',
+  'GUILD_MESSAGES',
+  'GUILD_MESSAGE_REACTIONS',
+];
+const client = new Discord.Client({
+  intents: intents,
+  ws: { intents: intents },
+});
+
+// connects to database
+connectDB();
+
+// creates a collection of commands and events
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
